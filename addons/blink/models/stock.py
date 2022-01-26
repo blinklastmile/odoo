40f3faf6566b9d00c4264e9757ad39fca6f22dba
marcoturetta <marco.turetta@blinklastmile.com>
2022-01-26 10:16:30 +0100

    feat: custom field label

--- addons/blink/models/stock.py
+++ addons/blink/models/stock.py
@@ -9,18 +9,6 @@ from odoo.tools.sql import column_exists, create_column
 class StockPicking(models.Model):
     _inherit = 'stock.picking'
 
-    sale_id = fields.Many2one(related="group_id.sale_id", string="Sales Order", store=True, readonly=False)
+    sale_order = fields.Many2one(related="group_id.sale_id", string="Sales Order", store=True, readonly=False)
 
     # deliv_label = fields.Char(related="sale_id.deliv_label")
-
-    def _auto_init(self):
-        """
-        Create related field here, too slow
-        when computing it afterwards through _compute_related.
-
-        Since group_id.sale_id is created in this module,
-        no need for an UPDATE statement.
-        """
-        if not column_exists(self.env.cr, 'stock_picking', 'sale_id'):
-            create_column(self.env.cr, 'stock_picking', 'sale_id', 'int4')
-        return super()._auto_init()
