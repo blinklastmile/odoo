e25e9eaacd6c65f765b5841ac05316922c13705a
armansabyrov <arman.sabyrov@blinklastmile.com>
2022-07-27 17:26:50 +0600

    fix: updated request for package sync

--- addons/blink/models/stock.py
+++ addons/blink/models/stock.py
@@ -15,36 +15,39 @@ class StockPicking(models.Model):
 
     def action_sync_packages(self):
         stock_picking = super(StockPicking, self)
-
-        print(stock_picking.move_line_ids)
-        # dict data collect
-        request_body = {}
+        request_body = []
+        already_in = []
         for line in stock_picking.move_line_ids:
             package = self.env['stock.quant.package'].browse([line['result_package_id'].id])
             # self.deliv_label = "http://localhost:8000/admin/database/delivery/63b51efa-8429-4b04-ae59-4781bd76faec"
+
+            if package.name in already_in:
+                continue
             try:
-                print("name", package.name)
-                request_body = {
+                package_data = {
                     'width': package.package_type_id.width,
                     'height': package.package_type_id.height,
                     'depth': package.package_type_id.packaging_length,
                     'weight': package.package_type_id.max_weight,
-                    'delivery': self.deliv_label.split("/")[-1]
+                    'delivery': self.deliv_label.split("/")[-1],
+                    'external_id': package.id
                 }
-
-                print(request_body)
-                url = self.deliv_label.split("admin")[0]
-
-                if package.x_initial_type is not None and package.x_initial_type == package.package_type_id:
-                    print('updating package')
-                    request_body['code'] = package.name
-                    response = requests.patch(url + "api/admin/packages/" + package.id, data=request_body)
-                    print(response.content)
-                else:
-                    print('creating package')
-                    response = requests.post(url + "api/admin/packages/", data=request_body)
-                    print(response.text)
-                    package.name = json.loads(response.text)['code']
-
+                if package.external_id:
+                    package_data['uuid'] = package.external_id
+                already_in.append(package.name)
+                request_body.append(package_data)
             except Exception as e:
                 print(str(e))
+
+        url = self.deliv_label.split("admin")[0]
+        response = requests.post(url + "api/admin/deliveries/" + self.deliv_label.split("/")[-1] + "/sync_packages/",
+                                  json=request_body)
+        new_packages = json.loads(response.text)
+        for new_package in new_packages:
+            package = self.env['stock.quant.package'].browse([int(new_package['external_id'])])
+            if package:
+                package.name = new_package['code']
+                if 'uuid' in new_package:
+                    package.external_id = new_package['uuid']
+
+
